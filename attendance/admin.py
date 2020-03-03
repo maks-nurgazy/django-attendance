26308b73780e87bfca579b1c54f8a8ad1b72b428
maks-nurgazy <maksatbek.bolushov@gmail.com>
2020-03-03 23:38:12 +0600

    database added

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Teacher, Course, Student, Fingerprint, Attendance, Record
 
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Fingerprint)
+admin.site.register(Attendance)
+admin.site.register(Record)
--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,58 @@
+from datetime import date
+
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    firstName = models.CharField("Имя", max_length=50)
+    lastName = models.CharField("Фамилия", max_length=50)
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
+
+
+class Course(models.Model):
+    code = models.CharField("Код урока", max_length=15)
+    name = models.CharField("Название урока", max_length=50)
+    teacher = models.ForeignKey(Teacher, verbose_name="Инструктор", related_name="teachers_course",
+                                on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Student(models.Model):
+    rollNumber = models.CharField("ID студента", max_length=15)
+    firstName = models.CharField("Имя", max_length=50)
+    lastName = models.CharField("Фамилия", max_length=50)
+    course = models.ManyToManyField(Course, verbose_name="Курсы", related_name="course_student")
+
+    def __str__(self):
+        return f"{self.firstName} {self.lastName}"
+
+    class Meta:
+        verbose_name = "Студент"
+        verbose_name_plural = "Студенты"
+
+
+class Fingerprint(models.Model):
+    samples = models.ImageField("Отпечаток", upload_to="fingerprints/")
+    student = models.ForeignKey(Student, verbose_name="Студент", on_delete=models.CASCADE)
+
+
+class Attendance(models.Model):
+    date = models.DateField("Дата", default=date.today)
+    course = models.ForeignKey(Course, verbose_name="названия урока", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.course} - {self.date}"
+
+
+class Record(models.Model):
+    student = models.ForeignKey(Student, verbose_name="Студент", on_delete=models.CASCADE)
+    status = models.BooleanField("Статус", default=True)
+    attendance = models.ForeignKey(Attendance, verbose_name="посещаемость", on_delete=models.CASCADE)
